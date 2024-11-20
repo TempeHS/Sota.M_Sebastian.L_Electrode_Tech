628017a75811ba9198f2716bb8ba3213e3875cf5
sotama <sota.ma@education.nsw.gov.au>
2024-11-20 12:25:49 +1100

    Update Electrode_Tech.ino

--- Electrode.Tech/Electrode_Tech/Electrode_Tech.ino
+++ Electrode.Tech/Electrode_Tech/Electrode_Tech.ino
@@ -1,3 +1,10 @@
+Sota:
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue
 US sensor pin 7
+
+Sebastian:
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue
 Servo 1 pin 3
 Servo 2 pin 4
+
+2x Servo
