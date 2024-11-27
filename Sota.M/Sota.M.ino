d2335ea17759eb81fd3eb3a40e2a1b7e6b9bbdd9
SebbyLam <sebastian.lam1@education.nsw.gov.au>
2024-11-27 15:04:31 +1100

    Ultrasonic works

--- Sota.M/Sota.M.ino
+++ Sota.M/Sota.M.ino
@@ -1,19 +1,38 @@
-#include "<Ultrasonic.h>"
+#include "Ultrasonic.h"
+#include <Servo.h>
 
-//#include "Ultrasonic.h"
+Servo myservo;
+Ultrasonic dingleberry(5);
 
-Ultrasonic sensorOne;
-void setup()
-{
-  sensorOne.attach(4);
- Serial.begin(9600);
+#define servoPin 7
+
+int distance;
+//unsigned static int potpin = A2;
+
+unsigned long previousMillis;
+unsigned long gateTime = (100);
+void setup() {
+  myservo.attach(servoPin);
+
+  Serial.begin(9600);
 }
-void loop()
-{
-  long RangeInCentimeters;
 
-  RangeInCentimeters = myUltrasonicSensor.MeasureInCentimeters(); // two measurements should keep an interval
-  Serial.print(RangeInCentimeters);//0~400cm
-  Serial.println(" cm");
-  delay(250);
+void loop() {
+unsigned long currentMillis = millis();
+  distance = dingleberry.distanceRead();
+  Serial.println(dingleberry.distanceRead());
+
+  if(dingleberry.distanceRead() >=40)
+  {
+    previousMillis = currentMillis;
+  
+     
+    myservo.write(0);
+
+     
+  } else 
+  {
+    if(currentMillis - previousMillis >=gateTime)
+    myservo.write(180);
+  }
 }
